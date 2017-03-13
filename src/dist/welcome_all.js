48e1f4acc4ac1e0442f55df39bced4a35aa0fa8b
evelyn_zhang <1054982653@qq.com>
2017-03-13 18:29:52 +0800

    rank

--- src/dist/welcome_all.js
+++ src/dist/welcome_all.js
@@ -741,12 +741,13 @@ Api = {
                 console.log(data);
 
                 wx.config({
-                    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                     appId: data.appId, // 必填，公众号的唯一标识
                     timestamp: data.timestamp, // 必填，生成签名的时间戳
                     nonceStr: data.nonceStr, // 必填，生成签名的随机串
                     signature: data.signature,// 必填，签名，见附录1
                     jsApiList: ['onMenuShareAppMessage','onMenuShareTimeline'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                    
                 });
 
                 wx.onMenuShareAppMessage({
