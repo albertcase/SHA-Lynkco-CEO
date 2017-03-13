909b53050b31d139208e6e7c6eb5d4837f48d0f1
evelyn_zhang <1054982653@qq.com>
2017-03-13 23:41:57 +0800

    weixin

--- src/dist/rank_all.js
+++ src/dist/rank_all.js
@@ -598,7 +598,7 @@ $(document).ready(function(){
                 weixinshare({
                     title1: '亮出身份，用分数标榜态度，LYNK & CO邀你来做CEO!',
                     des: '用实力让情怀落地，用分数为自己说话',
-                    link: window.location.origin,
+                    link: window.location.href,
                     img: window.location.origin+'/src/images/share.jpg'
                 },function(){
 
