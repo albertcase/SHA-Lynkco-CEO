e8faaaaeb771fa5d3d870304e3c0c03d6498fe38
evelyn_zhang <1054982653@qq.com>
2017-03-13 23:51:09 +0800

    weixinshare

--- src/dist/rank_all.js
+++ src/dist/rank_all.js
@@ -589,7 +589,7 @@ $(document).ready(function(){
                         console.log(data);
                         //weixinshare
                         weixinshare({
-                            title1: '看我CEO测试高达'+data.total+'分，实力碾压你的双商！',
+                            title1: '看我CEO测试高达'+data.msg.total+'分，实力碾压你的双商！',
                             des: '用实力让情怀落地，用分数为自己说话',
                             link: window.location.origin+'/rank?id='+data.msg,
                             img: window.location.origin+'/src/images/share.jpg'
